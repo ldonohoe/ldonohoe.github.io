8252b05cdf324bac78a65d192dac9f415dd40d83
ldonohoe <donohl@rpi.edu>
2019-07-07 01:26:16 -0400

    Created about, contact, and projects pages. Made popup menu. Did some recoloring.

--- func.js
+++ func.js
@@ -2,4 +2,12 @@
 
 function myFunc(x) {
 	x.classList.toggle("change")
+
+	var y = document.getElementById("links");
+	if (y.style.display === "block") {
+		y.style.display="none";
+	}
+	else {
+		y.style.display = "block";
+	}
 }
